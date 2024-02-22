ec6600e5b6e4d11e2398af3c558a6904312bfb56
Tuhin531 <mdtuhinhossain153@gmail.com>
2024-02-22 20:20:22 +0100

    add triangle backend code

---
+++ js/triangle.js
@@ -0,0 +1,23 @@
+document.getElementById('triangle-calculate').addEventListener('click', function(){
+    const baseInput = document.getElementById('base-input');
+    const BaseInputString = baseInput.value;
+    const NewBaseInput = parseFloat(BaseInputString);
+    baseInput.value = '';
+    //  console.log(NewBaseInput);
+
+    const heightInput = document.getElementById('height-input');
+    const heightInputString = heightInput.value;
+    const NewHeightInput = parseFloat(heightInputString);
+    //  console.log( NewHeightInput);
+    heightInput.value = '';
+
+    const calculateValue = 0.5 * NewBaseInput * NewHeightInput;
+
+   
+    const finalOutput= document.getElementById('calculate-value-show');
+    const finalOutputString = finalOutput.value;
+    const NewFinalOutput = parseFloat(finalOutputString);
+    // console.log(NewFinalOutput);
+    finalOutput.innerText = calculateValue;
+
+})
